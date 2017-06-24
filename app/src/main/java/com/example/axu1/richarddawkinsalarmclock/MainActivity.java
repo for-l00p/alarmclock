8f0c749757a34fc39749e064396bcdeb46de253d
lauralindy <laura.f.lin@gmail.com>
2017-06-24 19:14:51 -0400

    stuff

---
+++ app/src/main/java/com/example/axu1/richarddawkinsalarmclock/MainActivity.java
@@ -0,0 +1,190 @@
+package com.example.axu1.richarddawkinsalarmclock;
+
+import android.annotation.TargetApi;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    AlarmManager alarmManager;
+    private PendingIntent pending_intent;
+
+    private TimePicker alarmTimePicker;
+    private TextView alarmTextView;
+
+    private AlarmReceiver alarm;
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    MainActivity inst;
+    Context context;
+    Intent myIntent;
+
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+
+
+    final Calendar calendar = Calendar.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        this.context = this;
+
+        //alarm = new AlarmReceiver();
+        alarmTextView = (TextView) findViewById(R.id.alarmText);
+
+        myIntent = new Intent(this.context, AlarmReceiver.class);
+
+        // Get the alarm manager service
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        // set the alarm to the time that you picked
+
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+
+
+
+
+        Button start_alarm= (Button) findViewById(R.id.start_alarm);
+        start_alarm.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.M)
+
+            @Override
+            public void onClick(View v) {
+
+                calendar.add(Calendar.SECOND, 3);
+                //setAlarmText("You clicked a button");
+
+                final int hour = alarmTimePicker.getCurrentHour();
+                final int minute = alarmTimePicker.getCurrentMinute();;
+
+                Log.e("MyActivity", "In the receiver with " + hour + " and " + minute);
+                setAlarmText("You clicked a " + hour + " and " + minute);
+
+
+                calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+                calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+
+                myIntent.putExtra("extra", "yes");
+                pending_intent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
+
+
+                // now you should change the set Alarm text so it says something nice
+
+
+                setAlarmText("Alarm set to " + hour + ":" + minute);
+                //Toast.makeText(getApplicationContext(), "You set the alarm", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        Button stop_alarm= (Button) findViewById(R.id.stop_alarm);
+        stop_alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                myIntent.putExtra("extra", "no");
+                sendBroadcast(myIntent);
+
+                alarmManager.cancel(pending_intent);
+                setAlarmText("Alarm canceled");
+                //setAlarmText("You clicked a " + " canceled");
+            }
+        });
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    myIntent.putExtra("extra", "no");
+                    sendBroadcast(myIntent);
+
+                    alarmManager.cancel(pending_intent);
+                    setAlarmText("Alarm canceled");
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void setAlarmText(String alarmText) {
+        alarmTextView.setText(alarmText);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.e("MyActivity", "on Destroy");
+    }
+
+
+}
